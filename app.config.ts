d972719ee1ea4c770028c05093bceafa9ebcc127
shahank42 <shounakghosh42@gmail.com>
2025-04-16 19:43:35 +0530

    chore: update server preset to cloudflare-pages and add unenv configuration

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "@tanstack/start/config";
+import { cloudflare } from "unenv";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
-    preset: "netlify",
+    preset: "cloudflare-pages",
+    unenv: cloudflare,
   },
   vite: {
     plugins: [
