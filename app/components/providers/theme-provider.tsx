54124c4049208de9f60c2da1ccaed8b76ff0de79
shahank42 <shounakghosh42@gmail.com>
2025-04-16 20:26:06 +0530

    chore: react 19 codemods

--- app/components/providers/theme-provider.tsx
+++ app/components/providers/theme-provider.tsx
@@ -57,9 +57,9 @@ export function ThemeProvider({
   };
 
   return (
-    <ThemeProviderContext.Provider {...props} value={value}>
+    (<ThemeProviderContext {...props} value={value}>
       {children}
-    </ThemeProviderContext.Provider>
+    </ThemeProviderContext>)
   );
 }
 
--- app/components/ui/label.tsx
+++ app/components/ui/label.tsx
@@ -4,18 +4,22 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-const Label = React.forwardRef<HTMLLabelElement, React.LabelHTMLAttributes<HTMLLabelElement>>(
-  ({ className, ...props }, ref) => (
-    <label
-      ref={ref}
-      className={cn(
-        "text-sm font-medium leading-4 text-foreground peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-        className,
-      )}
-      {...props}
-    />
-  ),
-);
+const Label = (
+  {
+    ref,
+    className,
+    ...props
+  }: React.LabelHTMLAttributes<HTMLLabelElement> & {
+    ref: React.RefObject<HTMLLabelElement>;
+  }
+) => (<label
+  ref={ref}
+  className={cn(
+    "text-sm font-medium leading-4 text-foreground peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+    className,
+  )}
+  {...props}
+/>);
 Label.displayName = "Label";
 
 export { Label };
