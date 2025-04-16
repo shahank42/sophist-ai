dc17877548a6d85afc466b281439eb3bfbe13f4d
shahank42 <shounakghosh42@gmail.com>
2025-04-16 21:51:18 +0530

    chore: drizzle config update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./app/lib/server/db/drizzle",
-  schema: "./app/lib/server/db/schema/index.ts",
+  out: "./src/lib/server/db/drizzle",
+  schema: "./src/lib/server/db/schema/index.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
