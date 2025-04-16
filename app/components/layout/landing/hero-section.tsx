37d5e8438970bb76ef5ba56ea0d117ae58010e3b
shahank42 <shounakghosh42@gmail.com>
2025-04-16 15:51:14 +0530

    chore: enhance HeroSection with loading state and Google sign-in button styles

--- app/components/layout/landing/hero-section.tsx
+++ app/components/layout/landing/hero-section.tsx
@@ -1,8 +1,8 @@
 // components/landing/HeroSection.tsx
-import React from "react";
+import React, { useState } from "react";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { motion } from "framer-motion";
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, Loader2 } from "lucide-react";
 import { authClient } from "@/lib/utils/auth-client";
 import { getRouteApi, Link } from "@tanstack/react-router";
 import { cn } from "@/lib/utils";
@@ -10,6 +10,7 @@ import { DeviconGoogleWordmark } from "@/components/icons/devicon-google-wordmar
 
 const HeroSection = () => {
   const { user } = getRouteApi("__root__").useRouteContext();
+  const [loading, setLoading] = useState(false);
 
   return (
     <section
@@ -47,27 +48,114 @@ const HeroSection = () => {
           >
             {!user ? (
               <Button
-                size="lg"
-                className="w-full sm:w-auto py-5 sm:py-7 px-8 sm:px-12 text-lg sm:text-xl font-bold shadow-lg flex items-center justify-center gap-3"
+                variant="default"
+                className={cn(
+                  "w-full sm:w-auto py-5 sm:py-7 px-8 sm:px-12 text-lg sm:text-xl font-bold shadow-lg flex items-center justify-center gap-3 rounded-full"
+                )}
+                disabled={loading}
                 aria-label="Log in with Google"
                 onClick={async () => {
-                  await authClient.signIn.social({
-                    provider: "google",
-                    callbackURL: "/study",
-                  });
+                  await authClient.signIn.social(
+                    {
+                      provider: "google",
+
+                      callbackURL: "/study",
+                    },
+
+                    {
+                      onRequest: (ctx) => {
+                        setLoading(true);
+                      },
+
+                      onResponse: (ctx) => {
+                        setLoading(false);
+                      },
+                    }
+                  );
                 }}
               >
-                <span className="flex items-center gap-2">
-                  Log in with Google
-                  {/* <DeviconGoogleWordmark className="w-full" /> */}
-                </span>
+                {/* {loading ? (
+                  <Loader2 size={16} className="animate-spin" />
+                ) : ( */}
+                <>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="0.98em"
+                    height="1em"
+                    viewBox="0 0 256 262"
+                  >
+                    <path
+                      fill="#4285F4"
+                      d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
+                    ></path>
+
+                    <path
+                      fill="#34A853"
+                      d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
+                    ></path>
+
+                    <path
+                      fill="#FBBC05"
+                      d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z"
+                    ></path>
+
+                    <path
+                      fill="#EB4335"
+                      d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
+                    ></path>
+                  </svg>
+                  Sign in with Google
+                </>
+                {/* )} */}
               </Button>
             ) : (
+              // <Button
+              //   size="lg"
+              //   // className="w-full sm:w-auto py-5 sm:py-7 px-8 sm:px-12 text-lg sm:text-xl font-bold shadow-lg flex items-center justify-center gap-3"
+              //   className="flex items-center bg-white border border-button-border-light rounded-full p-0.5 pr-4 "
+              //   aria-label="Log in with Google"
+              //   onClick={async () => {
+              //     await authClient.signIn.social({
+              //       provider: "google",
+              //       callbackURL: "/study",
+              //     });
+              //   }}
+              // >
+              //   <div className="flex items-center justify-center bg-white w-9 h-9 rounded-full">
+              //     <svg
+              //       xmlns="http://www.w3.org/2000/svg"
+              //       viewBox="0 0 24 24"
+              //       className="w-5 h-5"
+              //     >
+              //       <title>Sign in with Google</title>
+              //       <desc>Google G Logo</desc>
+              //       <path
+              //         d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+              //         className="fill-google-logo-blue"
+              //       ></path>
+              //       <path
+              //         d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+              //         className="fill-google-logo-green"
+              //       ></path>
+              //       <path
+              //         d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+              //         className="fill-google-logo-yellow"
+              //       ></path>
+              //       <path
+              //         d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+              //         className="fill-google-logo-red"
+              //       ></path>
+              //     </svg>
+              //   </div>
+              //   <span className="text-sm text-google-text-gray tracking-wider">
+              //     Log in with Google
+              //   </span>
+              // </Button>
               <Link
                 to={"/study"}
                 className={cn(
                   buttonVariants({ variant: "default", size: "lg" }),
-                  "w-full sm:w-auto py-5 sm:py-7 px-8 sm:px-12 text-lg sm:text-xl font-bold shadow-lg"
+                  "w-full sm:w-auto py-5 sm:py-7 px-8 sm:px-12 text-lg sm:text-xl font-bold shadow-lg rounded-full"
                 )}
                 aria-label="Get Started"
               >
