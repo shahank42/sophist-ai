e09bd791e93e7f7a6ee65e14ebcb640b3d0b210b
shahank42 <shounakghosh42@gmail.com>
2025-04-17 17:54:17 +0530

    fix: button z-index

--- src/routes/study/index.tsx
+++ src/routes/study/index.tsx
@@ -41,8 +41,8 @@ function RouteComponent() {
       {user ? <HomeSidebar user={user} /> : <></>}
       <SidebarInset>
         {user ? (
-          <div className="py-3 px-3 flex w-full justify-between z-100">
-            <SidebarTrigger className="" />
+          <div className="py-3 px-3 flex w-full justify-between">
+            <SidebarTrigger className="z-20" />
             <ThemeToggle />
           </div>
         ) : (
@@ -57,7 +57,7 @@ function RouteComponent() {
 
         <div
           aria-hidden
-          className="absolute inset-0 isolate opacity-65 contain-strict block rounded-xl"
+          className="absolute inset-0 isolate opacity-65 contain-strict block md:rounded-xl"
         >
           <div className="w-140 h-320 -translate-y-87.5 absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
           <div className="h-320 absolute left-0 top-0 w-60 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]" />
