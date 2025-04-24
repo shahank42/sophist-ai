11a3c701dbfd5f009d63d102099a5a9eb8754487
shahank42 <shounakghosh42@gmail.com>
2025-04-24 21:01:56 +0530

    feat: ip address api route

---
+++ src/routes/api/geo.ts
@@ -0,0 +1,9 @@
+import { json } from "@tanstack/react-start";
+import { createAPIFileRoute } from "@tanstack/react-start/api";
+
+export const APIRoute = createAPIFileRoute("/api/geo")({
+  GET: ({ request, params }) => {
+    const ip = request.headers.get("X-Forwarded-For");
+    return json({ ip });
+  },
+});
