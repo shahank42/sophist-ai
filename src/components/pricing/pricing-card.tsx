49fec9977b9e965831b54491f7dcd4aa65503069
shahank42 <shounakghosh42@gmail.com>
2025-04-24 20:21:47 +0530

    feat: log user country code in PricingCard component

--- src/components/pricing/pricing-card.tsx
+++ src/components/pricing/pricing-card.tsx
@@ -49,6 +49,7 @@ export function PricingCard({
 }: PricingCardProps) {
   // const { countryCode, isLoading, isError, error } = useCountryCode();
   const { userCountryCode } = getRouteApi("/buy/").useLoaderData();
+  console.log("user country code", userCountryCode);
 
   const {
     data: creditBundles,
