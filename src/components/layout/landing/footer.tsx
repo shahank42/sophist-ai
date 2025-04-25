1e0f0ffcce1a39b709b245168eabd5372fa5174f
shahank42 <shounakghosh42@gmail.com>
2025-04-25 12:16:51 +0530

    chore: added fb link

--- src/components/layout/landing/footer.tsx
+++ src/components/layout/landing/footer.tsx
@@ -84,8 +84,8 @@ export default function Footer() {
                 ></path>
               </svg>
             </a>
-            {/* <a
-              href="/"
+            <a
+              href="https://www.facebook.com/share/166PrqoRX5/"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="Facebook"
@@ -103,7 +103,7 @@ export default function Footer() {
                   d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"
                 ></path>
               </svg>
-            </a> */}
+            </a>
             <a
               href="https://www.instagram.com/sophist_ai"
               target="_blank"
