3ec070ffa191341f5500f6e6ef41a49f58f872bc
shahank42 <shounakghosh42@gmail.com>
2025-04-25 12:15:12 +0530

    fix: herobg fixes

--- src/components/layout/landing/hero-section-2.tsx
+++ src/components/layout/landing/hero-section-2.tsx
@@ -70,11 +70,18 @@ export default function HeroSection() {
               className="absolute inset-0 -z-20"
             >
               <img
-                src="/landing/herobg.jpg"
+                src="/landing/herobg.webp"
                 alt="background"
-                className="absolute inset-x-0 top-56 -z-20 hidden lg:top-32 dark:block"
+                className="hidden md:dark:block absolute inset-x-0 -z-20 lg:top-32"
                 width="3276"
                 height="4095"
+              />
+              <img
+                src="/landing/herobg_mobile.webp"
+                alt="background"
+                className="md:hidden absolute inset-x-0 -z-20 top-56 lg:top-32 dark:block"
+                width="412"
+                height="926"
               />
             </AnimatedGroup>
             <div className="absolute inset-0 -z-10 size-full [background:radial-gradient(125%_125%_at_50%_100%,transparent_0%,var(--color-background)_75%)]"></div>
