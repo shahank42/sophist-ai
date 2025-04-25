42100d16a7606e78851f4c2a84c2a043b03b9ff9
shahank42 <shounakghosh42@gmail.com>
2025-04-25 11:06:34 +0530

    fix: bottom padding

--- src/components/layout/landing/hero-section-2.tsx
+++ src/components/layout/landing/hero-section-2.tsx
@@ -41,7 +41,7 @@ export default function HeroSection() {
           <div className="h-320 -translate-y-87.5 absolute left-0 top-0 w-60 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
         </div>
         <section>
-          <div className="relative pt-32 md:pt-36 pb-16 md:pb-32">
+          <div className="relative pt-32 md:pt-36">
             <AnimatedGroup
               variants={{
                 container: {
