cabf6b9f16a11b114547813571e75dd5e88e9748
shahank42 <shounakghosh42@gmail.com>
2025-05-05 05:27:40 +0530

    feat(feedback): added user feedback using better-auth-kit plugin!

--- src/components/user-details-select.tsx
+++ src/components/user-details-select.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronsUpDown, LogOut } from "lucide-react";
+import { ChevronsUpDown, LogOut, NotebookPen } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -23,6 +23,11 @@ import { useQuery, useQueryClient, useSuspenseQuery } from "@tanstack/react-quer
 import { getRouteApi, useRouter } from "@tanstack/react-router";
 import { User } from "better-auth";
 import { getUserQueryOptions } from "@/routes/__root";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
+import { Textarea } from "./ui/textarea";
+import FeedbackForm from "./forms/feedback-form";
+import { useState } from "react";
 
 function UserAvatar({ user }: { user: User }) {
   return (
@@ -45,8 +50,10 @@ export function UserDetailsSelect() {
   const router = useRouter();
   // const { user } = getRouteApi("__root__").useRouteContext();
   const { data: user } = useSuspenseQuery(getUserQueryOptions);
-  const queryClient = useQueryClient()
-  
+  const queryClient = useQueryClient();
+
+  const [openFeedbackDialog, setOpenFeedbackDialog] = useState(false);
+
 
   // const { data: userCredits } = useQuery(getUserCreditsQueryOptions(user!.id));
 
@@ -56,79 +63,83 @@ export function UserDetailsSelect() {
 
   return (
     <SidebarMenu>
-      <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground cursor-pointer"
-            >
-              <UserAvatar user={user} />
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
-              </div>
-              <ChevronsUpDown className="ml-auto size-4" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
-            align="end"
-            sideOffset={4}
-          >
-            <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+      <Dialog open={openFeedbackDialog} onOpenChange={setOpenFeedbackDialog}>
+
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton
+                size="lg"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground cursor-pointer"
+              >
                 <UserAvatar user={user} />
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">{user.name}</span>
                   <span className="truncate text-xs">{user.email}</span>
                 </div>
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              {/* {userSubscription.isPro ? ( */}
-              {/* <DropdownMenuLabel className="flex items-center gap-2">
+                <ChevronsUpDown className="ml-auto size-4" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+              side={isMobile ? "bottom" : "right"}
+              align="end"
+              sideOffset={4}
+            >
+              <DropdownMenuLabel className="p-0 font-normal">
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  <UserAvatar user={user} />
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">{user.name}</span>
+                    <span className="truncate text-xs">{user.email}</span>
+                  </div>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                {/* {userSubscription.isPro ? ( */}
+                {/* <DropdownMenuLabel className="flex items-center gap-2">
                 <Premium className="text-blue-500 size-4" />
                 <span>Credits: {user.credits}</span>
               </DropdownMenuLabel> */}
-              {/* ) : ( */}
-              {/* <DropdownMenuItem asChild>
-                <Link
-                  to="/buy"
-                  className="flex items-center justify-between w-full cursor-pointer"
-                >
-                  <div className="flex items-center gap-2">
-                    <CoinsIcon className="text-primary size-4" />
-                    <span className="font-medium text-sm">Credits</span>
-                  </div>
-                  <Badge
-                    variant="outline"
-                    className="font-semibold text-primary text-md"
-                  >
-                    {userCredits?.credits}
-                  </Badge>
-                </Link>
-              </DropdownMenuItem> */}
-              {/* )} */}
-            </DropdownMenuGroup>
-            {/* <DropdownMenuSeparator /> */}
-            <DropdownMenuItem
-              className="cursor-pointer"
-              onClick={async () => {
-                await authClient.signOut();
-                await queryClient.invalidateQueries({queryKey: ["user"]});
-                await router.navigate({to: "/"});
-              }}
+                {/* ) : ( */}
+
+
+                <DialogTrigger asChild>
+                  <DropdownMenuItem>
+                    <NotebookPen /> Feedback
+                  </DropdownMenuItem>
+                </DialogTrigger>
+
+
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className="cursor-pointer"
+                onClick={async () => {
+                  await authClient.signOut();
+                  await queryClient.invalidateQueries({ queryKey: ["user"] });
+                  await router.navigate({ to: "/" });
+                }}
               // onClick={async () => await signOutFn()}
-            >
-              <LogOut />
-              Log out
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </SidebarMenuItem>
+              >
+                <LogOut />
+                Log out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarMenuItem>
+
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="text-xl">Share Your Feedback</DialogTitle>
+            <DialogDescription className="text-sm">
+              Your feedback helps us build a better study experience for everyone. Spotted a bug? Have a great idea? Tell us everything here!
+            </DialogDescription>
+          </DialogHeader>
+          <FeedbackForm setOpenFeedbackDialog={setOpenFeedbackDialog} />
+        </DialogContent>
+      </Dialog>
     </SidebarMenu>
   );
 }
--- src/lib/utils/auth-client.ts
+++ src/lib/utils/auth-client.ts
@@ -1,7 +1,9 @@
+import { feedbackClient } from "@better-auth-kit/feedback";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: process.env.BETTER_AUTH_URL as string,
+  plugins: [feedbackClient()],
 });
 
 export type Session = typeof authClient.$Infer.Session;
